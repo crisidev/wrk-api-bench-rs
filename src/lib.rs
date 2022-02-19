e6f6207b966d0a5062146a20fe907da94f7686b2
Bigo <1781140+crisidev@users.noreply.github.com>
2022-02-19 18:12:36 +0000

    Move lua handling in its own file

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,14 @@ extern crate log;
 
 mod benchmark;
 mod error;
+mod lua;
 mod result;
 mod wrk;
 
 pub use benchmark::{Benchmark, BenchmarkBuilder, BenchmarkBuilderError};
 pub use error::WrkError;
+pub use lua::LuaScript;
 pub use result::{WrkResult, WrkResultBuilder, WrkResultBuilderError};
-pub use wrk::{Wrk, WrkBuilder, WrkBuilderError};
+pub use wrk::{Headers, Wrk, WrkBuilder, WrkBuilderError};
 
 pub(crate) type Result<T> = std::result::Result<T, WrkError>;
--- src/wrk.rs
+++ src/wrk.rs
@@ -1,76 +1,25 @@
 use std::{
     collections::HashMap,
-    env,
     fs::{self, File},
-    io::{BufReader, BufWriter, Read, Write},
+    io::{BufReader, BufWriter},
     ops::Sub,
     path::{Path, PathBuf},
     process::Command,
-    time::{Duration, SystemTime},
+    time::Duration,
 };
 
-use assert_cmd::prelude::OutputOkExt;
 use chrono::{DateTime, Duration as ChronoDuration, NaiveDateTime, Utc};
 use getset::{Getters, MutGetters, Setters};
-use rslua::lexer::Lexer;
 use serde::{Deserialize, Serialize};
-use tempfile::NamedTempFile;
 use url::Url;
 
 use crate::{
     benchmark::{Benchmark, BenchmarkBuilder},
     error::WrkError,
-    result::{Variance, WrkResult, WrkResultBuilder},
-    Result,
+    result::{Variance, WrkResult},
+    LuaScript, Result,
 };
 
-const LUA_DEFAULT_DONE_FUNCTION: &str = r#"
--- The done() function is called at the end of wrk execution
--- and allows us to produce a well formed JSON output, prefixed
--- by the string "JSON" which allows us to parse the wrk output
--- easily.
-done = function(summary, latency, requests)
-    local errors = summary.errors.connect
-        + summary.errors.read
-        + summary.errors.write
-        + summary.errors.status
-        + summary.errors.timeout
-    io.write("JSON")
-    io.write(string.format(
-        [[{
-    "requests": %d,
-    "errors": %d,
-    "successes": %d,
-    "requests_sec": %d,
-    "avg_latency_ms": %.2f,
-    "min_latency_ms": %.2f,
-    "max_latency_ms": %.2f,
-    "stdev_latency_ms": %.2f,
-    "transfer_mb": %.d,
-    "errors_connect": %d,
-    "errors_read": %d,
-    "errors_write": %d,
-    "errors_status": %d,
-    "errors_timeout": %d
-}
-]],
-        summary.requests,
-        errors,
-        summary.requests - errors,
-        summary.requests / (summary.duration / 1000000),
-        (latency.mean / 1000),
-        (latency.min / 1000),
-        (latency.max / 1000),
-        (latency.stdev / 1000),
-        (summary.bytes / 1048576),
-        summary.errors.connect,
-        summary.errors.read,
-        summary.errors.write,
-        summary.errors.status,
-        summary.errors.timeout
-    ))
-end
-"#;
 const DATE_FORMAT: &str = "%Y-%m-%d-%H:%M:%S-%z";
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -155,74 +104,15 @@ pub struct Wrk {
 }
 
 impl Wrk {
-    fn lua_script_from_config(&self, uri: &str) -> Result<PathBuf> {
-        let request = format!(
-            r#"
--- The request() function is called by wrk on all requests
--- and allow us to configure things like headers, method, body, etc..
-request = function()
-    wrk.method = "{}"
-    wrk.body = "{}"
-    {}
-    return wrk.format("{}", "{}")
-end
-        "#,
-            self.method(),
-            self.body(),
-            self.lua_headers()?,
-            self.method(),
-            uri
-        );
-        let buffer = request + LUA_DEFAULT_DONE_FUNCTION;
-        let mut tmpfile = NamedTempFile::new()?;
-        tmpfile.write_all(buffer.as_bytes())?;
-        let result = tmpfile.path().to_path_buf();
-        tmpfile.keep().unwrap();
-        Ok(result)
-    }
-
-    fn lua_script_from_user(&self, lua_script: &Path) -> Result<PathBuf> {
-        let file = File::open(lua_script)?;
-        let mut reader = BufReader::new(file);
-        let mut buffer = String::new();
-        reader.read_to_string(&mut buffer)?;
-        let mut lexer = Lexer::new();
-        let tokens = lexer.run(&buffer).map_err(|e| WrkError::Lua(format!("{:?}", e)))?;
-        let buffer = buffer + LUA_DEFAULT_DONE_FUNCTION;
-        let mut tmpfile = NamedTempFile::new()?;
-        tmpfile.write_all(buffer.as_bytes())?;
-        Ok(tmpfile.path().to_path_buf())
-    }
-
-    fn lua_headers(&self) -> Result<String> {
-        let mut result = String::new();
-        for (k, v) in self.headers() {
-            result += &format!(r#"wrk.headers["{}"] = "{}"\n"#, k, v);
-        }
-        Ok(result)
-    }
-
-    fn lua_find_script(&self, uri: &str) -> Result<PathBuf> {
-        match self.user_script() {
-            Some(lua_script) => {
-                if !lua_script.exists() {
-                    error!(
-                        "Wrk Lua file {} not found in {}",
-                        env::current_dir().expect("unable to get current directory").display(),
-                        lua_script.display()
-                    );
-                    Err(WrkError::Lua("Wrk Lua file not found".to_string()))
-                } else {
-                    Ok(self.lua_script_from_user(lua_script)?)
-                }
-            }
-            None => Ok(self.lua_script_from_config(uri)?),
-        }
-    }
-
     fn wrk_args(&self, benchmark: &Benchmark) -> Result<Vec<String>> {
         let url = Url::parse(self.url())?;
-        let lua_script = self.lua_find_script(url.path())?;
+        let lua_script = LuaScript::render(
+            self.user_script().as_ref(),
+            url.path(),
+            self.method(),
+            self.headers(),
+            self.body(),
+        )?;
         Ok(vec![
             "-t".to_string(),
             benchmark.threads().to_string(),
